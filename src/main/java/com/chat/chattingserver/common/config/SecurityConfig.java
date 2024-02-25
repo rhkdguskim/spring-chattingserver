6c24c6307819ca4991e87fe6323a926f8e4df64b
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2024-02-25 18:26:47 +0900

    fix : cors policy fixed

--- src/main/java/com/chat/chattingserver/common/config/SecurityConfig.java
+++ src/main/java/com/chat/chattingserver/common/config/SecurityConfig.java
@@ -16,10 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig {
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .httpBasic().disable()
